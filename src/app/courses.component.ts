88661813021d3a8008d8c5dabcd36c8b4bc8843c
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-02-17 04:36:14 +0200

    class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,11 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-   <button class = "btn btn-primary">Save</button>
+   <button class = "btn btn-primary" [class.active]="isActive">Save</button>
   `
 })
 
 export class CoursesComponent {
+  isActive = true;
 
 }
