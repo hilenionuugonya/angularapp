d70a795d25b0c254573d3d0fb20bc557b7a3ce10
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-02-17 05:46:16 +0200

    template variables

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,13 +3,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <input (keyup.enter)="onKeyUp()"/>
+    <input #email (keyup.enter)="onKeyUp(email.value)"/>
   `
 })
 
 export class CoursesComponent {
- onKeyUp() {
-    console.log('ENTER was pressed');
+ onKeyUp(email) {
+    console.log(email);
  }
 
 
