dd0af41e67ba8b23584c1bf365b257969a10da85
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-02-17 04:27:41 +0200

    adding bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,17 +3,10 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <img [src]='imageUrl'/>
-    <table>
-      <tr>
-        <td [attr.colspan]='colSpan'></td>
-      </tr>
-    </table>
+   <button class = "btn btn-primary">Save</button>
   `
 })
 
 export class CoursesComponent {
-  imageUrl = '' ;
-  colSpan = 2;
 
 }
