2712a343115256f1f736db3363ba21f9dfdab1f4
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-02-20 05:52:20 +0200

    custom pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,22 +3,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-     {{course.title | uppercase}}<br/>
-    {{course.students | number}}<br/>
-    {{course.rating | number: '1.2-2'}}<br/>
-    {{course.price | currency:'N$':true}}<br/>
-    {{course.releaseDate | date:'shortDate'}}
+     {{text |summary:10}}
   `
 })
 
 export class CoursesComponent {
-  course = {
-    title: 'The Complete Angular Course',
-    rating: 4.9745,
-    students: 30123,
-    price: 190.95,
-    releaseDate: new Date(2016, 3, 1)
-  };
-
+  text = `
+  A pipe is a method used to pass information from one program process to another.
+  `
 
 }
