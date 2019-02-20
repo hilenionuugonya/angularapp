c714df347f9f483a3a601193b6fbcbc0767ba826
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-02-20 05:15:46 +0200

    pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,16 +3,22 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    <input [( ngModel)]="email" (keyup.enter)="onKeyUp()"/>
+     {{course.title | uppercase}}<br/>
+    {{course.students | number}}<br/>
+    {{course.rating | number: '1.2-2'}}<br/>
+    {{course.price | currency:'N$':true}}<br/>
+    {{course.releaseDate | date:'shortDate'}}
   `
 })
 
 export class CoursesComponent {
-  email = 'hileni@example.com';
-
- onKeyUp() {
-    console.log(this.email);
- }
+  course = {
+    title: 'The Complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  };
 
 
 }
