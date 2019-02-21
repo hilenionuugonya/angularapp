e8ea8682413fd9b1f1bff84eaf492a46f7717ee9
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-02-21 10:30:14 +0200

    title casing assignment completed

---
+++ src/app/casing/casing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-casing',
+  templateUrl: './casing.component.html',
+  styleUrls: ['./casing.component.css']
+})
+export class CasingComponent implements OnInit {
+  title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
