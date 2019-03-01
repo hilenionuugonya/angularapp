e18bfc325ccecfa9fb6024d388ddc0d48dce7fbf
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-03-01 04:37:23 +0200

    aliasing output properties

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
  @Input('isFavorite') isSelected: boolean;
- @Output() change = new EventEmitter();
+ @Output('change') click = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
@@ -15,7 +15,7 @@ export class FavoriteComponent implements OnInit {
 
   onClick() {
     this.isSelected = !this.isSelected;
-    this.change.emit({newValue: this.isSelected});
+    this.click.emit({newValue: this.isSelected});
   }
 
 }
