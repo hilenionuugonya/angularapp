0f2c4acef6a82f62317bf4920af5660accfcc707
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-03-13 03:38:38 +0200

    ngModel complete

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
+  log(x) { console.log(x); }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
