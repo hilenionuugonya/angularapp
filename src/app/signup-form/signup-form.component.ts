38d987b30f412c2e68ac422ec90ea4d413a04514
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-03-22 02:46:14 +0200

    nested formgroups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,20 +9,13 @@ import {UsernameValidators} from './username.validators';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('',
-      Validators.required,
-      UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   });
 
-  login() {
-    this.form.setErrors({
-      invalidLogin: true
-    });
-
-  }
-
-  get username(){
-    return this.form.get('username');
+  get username() {
+    return this.form.get('account.username');
   }
 }
