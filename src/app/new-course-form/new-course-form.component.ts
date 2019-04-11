9c216824014dec667cfcb3777e9fe69a0fcad439
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-04-11 04:11:12 +0200

    FormBuilder

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormArray, FormGroup, FormControl} from '@angular/forms';
+import {FormArray, FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-course-form',
@@ -7,21 +7,19 @@ import {FormArray, FormGroup, FormControl} from '@angular/forms';
   styleUrls: ['./new-course-form.component.css']
 })
 export class NewCourseFormComponent {
-  form = new FormGroup({
-    topics: new FormArray([])
-  });
+  form;
 
-  addTopic(topic: HTMLInputElement) {
-    (this.topics.push(new FormControl(topic.value)));
-    topic.value = '';
+  constructor (fb: FormBuilder){
+    this.form = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    });
+
   }
 
-  removeTopic(topic: FormControl) {
-    const index = this.topics.controls.indexOf(topic);
-    this.topics.removeAt(index);
-  }
 
-  get topics() {
-    return this.form.get('topics') as FormArray;
-  }
 }
