0d4de2b93a5b59e675ad9e7ac5723093950f0348
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-04-20 23:49:17 +0200

    removed error handling

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -11,17 +11,13 @@ import {HttpClient} from '@angular/common/http';
 export class PostsComponent implements OnInit {
   posts: any;
   private url = 'https://jsonplaceholder.typicode.com/posts' ;
-  private handleError(error: any): Promise<any> {
-    console.log('An error occured ', error);
-    return Promise.reject(error.message || error);
-}
 
 
   constructor( private http: HttpClient) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.http.get(this.url)
       .subscribe(response => {
         this.posts = response;
