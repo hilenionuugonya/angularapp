02083a908915d3a4863d9df8de7b7d466bf0eaec
Hileni Nuugonya <hilenionuugonya@gmail.com>
2019-05-03 00:56:08 +0200

    extracting a reusable error handling method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -6,39 +6,44 @@ import 'rxjs/add/observable/throw';
 import { AppErrorHandler} from '../common/app-error-handler';
 import {NotFoundError} from '../common/not-found-error';
 import {BadInput} from '../common/bad-input';
+import {post} from 'selenium-webdriver/http';
 
 @Injectable()
 export class PostService {
-   private url = 'https://jsonplaceholder.typicode.com/posts' ;
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url)
+      .catch(this.handleError);
   }
 
   createPost(post) {
     return this.http.post(this.url, JSON.stringify(post))
-      .catch((error: Response) => {
-        if (error.status === 404) {
-          return Observable.throw(new NotFoundError());
-        }
-        return Observable.throw(new AppErrorHandler());
-      });
+      .catch(this.handleError);
 
   }
 
   updatePost(post) {
-    return this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}));
+    return this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+      .catch(this.handleError);
   }
 
   deletePost(id) {
     return this.http.delete(this.url + '/' + id)
-      .catch((error: Response) => {
-        if (error.status === 404) {
-          return Observable.throw(new NotFoundError());
-        }
-        return Observable.throw(new AppErrorHandler());
-      });
+      .catch(this.handleError);
+  }
+
+  private handleError(error: Response) {
+    if (error.status === 400) {
+      return Observable.throw(new BadInput());
+
+      if (error.status === 404) {
+        return Observable.throw(new NotFoundError());
+      }
+      return Observable.throw(new AppErrorHandler());
+    }
   }
 }
